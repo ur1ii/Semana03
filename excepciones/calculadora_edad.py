42a0ea8e21e466d6da40d48b23bd0f013e909b10
HUMBERTO <hrruiz@uamv.edu.ni>
2026-08-31 10:38:09 -0600

    Calculadora Edad

--- excepciones/calculadora_edad.py
+++ excepciones/calculadora_edad.py
@@ -1,5 +1,12 @@
-try:
-    edad = int(input("Edad: "))
-    print("Edad registrada:", edad)
-except ValueError:
-    print("Ingresa un valor numérico")
+import os
+from colorama import Fore, Style
+import subprocess
+while True:
+    try:
+        subprocess.run("cls",shell=True)
+        edad = int(input("Edad: "))
+        break
+    except ValueError:
+        print("Ingresa un valor numérico")
+        subprocess("pause")
+print(Fore.GREEN + "Edad registrada:", edad, Style.RESET_ALL)
